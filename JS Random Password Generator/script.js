e4d42653fd6fddf224e97ccd3a2ad7c464541cac
Supritha <78655439+supzi-del@users.noreply.github.com>
2021-04-30 01:31:32 +0530

    strong pwd generator

---
+++ JS Random Password Generator/script.js
@@ -0,0 +1,19 @@
+//generate random password using for loop
+function generatePassword(){
+  var chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+?<>{}[]:";
+  var passwordLength=10;
+  var password="";
+  for(var i=0;i<passwordLength;i++){
+    var randomChar= Math.floor(Math.random() * chars.length)
+    password += chars.charAt(randomChar);
+  }
+  document.getElementById("password").value=password;
+}
+
+//copy to clipboard
+clipboard.onclick=function(){
+  
+  password.select();
+  
+  document.execCommand("copy");
+}
